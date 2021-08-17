60ec5f90307ded20149c86c942d9d62055a7cc73
Hilton De Meillon <hdemeillon@telenet.com.au>
2021-08-17 13:39:55 +1000

    cleaned up script and improved logic

--- installElasticAgent.ps1
+++ installElasticAgent.ps1
@@ -1,31 +1,39 @@
-$ServiceName = 'Elastic Endpoint'
-$AgentURL = 'https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.13.4-windows-x86_64.zip'
-$enrollmentToken = 'blah'
-$fleetUrl = 'https://something.elastic-cloud.com:443'
-$arrService = Get-Service -Name $ServiceName -ErrorAction SilentlyContinue
-$deploymentVersion = Get-ItemProperty 'HKLM:\Software\ElasticVersion'-ErrorAction SilentlyContinue |  Select-Object -ExpandProperty version -ErrorAction SilentlyContinue
 
-If (-not $deploymentVersion -eq 2) 
+$AgentURL = 'https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.13.4-windows-x86_64.zip'
+$enrollmentToken = 'somethingsecret'
+$fleetUrl = 'https://somethingsomething.fleet.eastus2.azure.elastic-cloud.com:443'
+$deploymentVersion = Get-ItemProperty 'HKLM:\Software\ElasticVersion'-ErrorAction SilentlyContinue |  Select-Object -ExpandProperty version -ErrorAction SilentlyContinue
+$currentversion =  4
+
+If ($deploymentVersion -lt $currentversion)
 {
-write-host 'Installing Elastic Agent'
-$randomString = -join ((65..90) + (97..122) | Get-Random -Count 15 | % {[char]$_})
-$tempDir = $env:TEMP + "\" + $randomString 
-mkdir $tempDir
-$zipFile = $tempDir + "\" + "elastic-agent.zip"
-write-host $zipFile
-Start-BitsTransfer -Source "https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.13.4-windows-x86_64.zip" -Destination $zipFile
-Expand-Archive -LiteralPath $zipFile -DestinationPath $tempDir
-$exeFile = $tempDir + "\" + "elastic-agent-7.13.4-windows-x86_64" + "\" + "elastic-agent.exe"
-Start-Process -FilePath $exeFile -ArgumentList "install","-f","--url=$fleetUrl","--enrollment-token=$enrollmentToken"
-New-Item -Path 'HKLM:\Software\ElasticVersion' -ErrorAction SilentlyContinue
-New-ItemProperty -Path 'HKLM:\Software\ElasticVersion' -Name "version" -Value 2 -Force -ErrorAction SilentlyContinue
+    write-host 'Installing Elastic Agent'
+
+    $randomString = -join ((65..90) + (97..122) | Get-Random -Count 15 | % {[char]$_})
+    $tempDir = $env:TEMP + "\" + $randomString 
+
+    mkdir $tempDir
+
+
+    $zipFile = $tempDir + "\" + "elastic-agent.zip"
+
+    write-host $zipFile
+    Start-BitsTransfer -Source "https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.13.4-windows-x86_64.zip" -Destination $zipFile
+    Expand-Archive -LiteralPath $zipFile -DestinationPath $tempDir
+
+    $exeFile = $tempDir + "\" + "elastic-agent-7.13.4-windows-x86_64" + "\" + "elastic-agent.exe"
+
+    Start-Process -FilePath $exeFile -ArgumentList "install","-f","--url=$fleetUrl","--enrollment-token=$enrollmentToken"
+    New-Item -Path 'HKLM:\Software\ElasticVersion' -ErrorAction SilentlyContinue
+    New-ItemProperty -Path 'HKLM:\Software\ElasticVersion' -Name "version" -Value $currentversion -Force -ErrorAction SilentlyContinue
 }
-elseif ($deploymentVersion -eq 2)
+
+elseif ($deploymentVersion -eq $currentversion)
 {
-write-host 'deployment is current, skipping'
+    write-host 'deployment is current, skipping'
 }
+
 else
 {
-write-host 'deployment is unknown, skipping'
+    write-host 'deployment is unknown, skipping'
 }
-
