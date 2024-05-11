1cf0dcdc9ee9efd2d940c5be8c01e1fd173f37c5
Hilton <hilt@threatbear.co>
2024-05-11 11:41:46 +1000

    Update installElasticAgent.ps1

--- installElasticAgent.ps1
+++ installElasticAgent.ps1
@@ -1,14 +1,11 @@
-$AgentURL = 'https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.2.3-windows-x86_64.zip'
+$AgentURL = 'https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.12.2-windows-x86_64.zip'
 # Fill these in before running
 $enrollmentToken = ''
 $fleetUrl = ''
-
 $elasticRunning = Get-Service | where {($_.Name -like "Elastic*") -and ($_.Status -eq "Running")}
-
 If ($elasticRunning -eq $null)
 {
     write-host 'Installing Elastic Agent'
-
     $randomString = -join ((65..90) + (97..122) | Get-Random -Count 15 | % {[char]$_})
     $tempDir = $env:TEMP + "\" + $randomString
     mkdir $tempDir
@@ -16,9 +13,7 @@ If ($elasticRunning -eq $null)
     write-host $zipFile
     Start-BitsTransfer -Source $AgentURL -Destination $zipFile
     Expand-Archive -LiteralPath $zipFile -DestinationPath $tempDir
-
-    $exeFile = $tempDir + "\" + "elastic-agent-8.2.3-windows-x86_64" + "\" + "elastic-agent.exe"
-
+    $exeFile = $tempDir + "\" + "elastic-agent-8.12.2-windows-x86_64" + "\" + "elastic-agent.exe"
     Start-Process -FilePath $exeFile -ArgumentList "install","-f","--url=$fleetUrl","--enrollment-token=$enrollmentToken"
 }
 else
